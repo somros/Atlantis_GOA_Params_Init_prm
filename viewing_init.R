7ec89ea7d23929b6dba1117ad22e5524ee2a9120
Alberto Rovellini <alberto.rovellini@gmail.com>
2022-03-29 16:24:19 -0700

    adding code to view plots of init we create

---
+++ viewing_init.R
@@ -0,0 +1,10 @@
+# view the init.nc that we produced
+
+library(shinyrAtlantis)
+library(tidyverse)
+
+bgm.file <-'../data/GOA_WGS84_V4_final.bgm'
+nc.file <- '../data/GOA_cb.nc'
+
+obj <- make.sh.init.object(bgm.file, nc.file)
+sh.init(obj)
